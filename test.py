32b78032e60211e3cc7b70a917f6c100e84c6f2e
JeanGuilmin <jean2guilmin@gmail.com>
2020-03-19 18:07:18 +0100

    Create and customize a window

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print('Hello World!')
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("snake")
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+
