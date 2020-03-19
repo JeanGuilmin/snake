f7b24927e464c567f3c36edfe88d4c5e7847910b
JeanGuilmin <jean2guilmin@gmail.com>
2020-03-19 18:51:58 +0100

    Change my window icon :)

--- test.py
+++ test.py
@@ -14,3 +14,13 @@ while run:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
+
+pygame.display.set_mode((800, 500))
+icon_32x32 = pygame.image.load("snake-icon.png").convert_alpha()
+pygame.display.set_icon(icon_32x32)
+ 
+exit = False
+while exit == False:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
