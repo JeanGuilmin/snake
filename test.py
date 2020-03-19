46d6e2ec5b386d5ec6cc2275305d1a6588d12fa5
PierreGuilmin <pierreguilmin@gmail.com>
2020-03-19 18:31:49 +0100

    Minor fixes to d66259b

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('snake')
 
 run = True
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,5 +14,3 @@ while run:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
-
-
